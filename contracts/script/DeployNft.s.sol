2f8b24dfa57bb73938cb7c4c5fb36d3583e2c8c9
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-06-05 09:07:36 +0530

    refactor deploy script

--- contracts/script/DeployNft.s.sol
+++ contracts/script/DeployNft.s.sol
@@ -3,11 +3,16 @@ pragma solidity ^0.8.0;
 
 import {Script} from "forge-std/Script.sol";
 import {NFT} from "src/NFT.sol";
+import {HelperConfig} from "script/HelperConfig.s.sol";
 
 contract DeployNFT is Script {
-    function run() external {
-        vm.startBroadcast();
-        NFT myNFT = new NFT(msg.sender);
+    function run() external returns (NFT, HelperConfig) {
+        HelperConfig config = new HelperConfig();
+        uint256 deployerKey = config.activeNetworkConfig();
+
+        vm.startBroadcast(deployerKey);
+        NFT nft = new NFT(msg.sender);
         vm.stopBroadcast();
+        return (nft, config);
     }
 }
