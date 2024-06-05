a71d6aa5bcaa6e0208510bd4d605120ca2b0577c
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-06-05 06:37:33 +0530

    deploy script

---
+++ contracts/script/DeployNft.s.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {Script} from "forge-std/Script.sol";
+import {NFT} from "src/NFT.sol";
+
+contract DeployNFT is Script {
+    function run() external {
+        vm.startBroadcast();
+        NFT myNFT = new NFT();
+        vm.stopBroadcast();
+    }
+}
