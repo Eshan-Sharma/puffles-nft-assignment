478505b0f64009f008d031bf2e0ed667ec684c44
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-06-05 08:51:28 +0530

    debug and add Enumerate

--- contracts/src/NFT.sol
+++ contracts/src/NFT.sol
@@ -2,18 +2,17 @@
 pragma solidity ^0.8.18;
 
 import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import {ERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract NFT is ERC721URIStorage, Ownable {
+contract NFT is ERC721, ERC721URIStorage, ERC721Enumerable, Ownable {
     ///////////////////
     //State Variables//
     ///////////////////
     uint256 public constant MAX_SUPPLY = 10;
     uint256 public constant MINT_PRICE = 0.01 ether;
     uint256 public constant MAX_PER_WALLET = 2;
-
-    uint256 public totalSupply = 0; //why?
 
     uint256 private _nextTokenId; //Auto Increment
 
@@ -31,14 +30,40 @@ contract NFT is ERC721URIStorage, Ownable {
     function safeMint(address _to, string calldata _uri) external payable returns (uint256) {
         uint256 tokenId = _nextTokenId++;
 
-        if (totalSupply >= MAX_SUPPLY) revert NFT__MaxSupplyReached();
-        if (walletMintCount[msg.sender] > MAX_PER_WALLET) revert NFT_MaxMintPerWalletReached();
+        if (totalSupply() >= MAX_SUPPLY) revert NFT__MaxSupplyReached();
+        if (walletMintCount[msg.sender] >= MAX_PER_WALLET) revert NFT_MaxMintPerWalletReached();
         if (msg.value < MINT_PRICE) revert NFT__InsufficientEtherSent();
 
         walletMintCount[msg.sender] += 1;
 
-        _safeMint(_to, tokenId);
-        _setTokenURI(tokenId, _uri);
+        _safeMint(_to, tokenId); //Emits Transfer(address(0), to, tokenId)
+        _setTokenURI(tokenId, _uri); // Emits MetadataUpdate(tokenId)
         return tokenId;
+    }
+
+    // The following functions are overrides required by Solidity.
+    function _update(address to, uint256 tokenId, address auth)
+        internal
+        override(ERC721, ERC721Enumerable)
+        returns (address)
+    {
+        return super._update(to, tokenId, auth);
+    }
+
+    function _increaseBalance(address account, uint128 value) internal override(ERC721, ERC721Enumerable) {
+        super._increaseBalance(account, value);
+    }
+
+    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 }
